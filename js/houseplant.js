79ec81e667c2b5f05a445620d05c1f16968b23b8
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-02-21 20:54:30 -0500

    Finished project

--- js/houseplant.js
+++ js/houseplant.js
@@ -3,52 +3,53 @@ let flowerHealth = 4;
 
 // get reference to div with plant 
 const flower = document.getElementById("flower");
-
 // Add event listeners to the three buttons to call each three functions named neglect, compliment, water
-document.getElementById("neglect").addEventListener("click", neglect);
-document.getElementById("compliment").addEventListener("click", compliment);
-document.getElementById("water").addEventListener("click", water);
+
 
 // write a neglect function that reduces the plant's health by 1 and changes the image acordingly
 function neglect() {
     flowerHealth--;
-    if (flowerHealth === 3) {
-        console.log(flower)
-        flower.style.backgroundPosition = "100px 0";
-    }
-    else if (flowerHealth === 2) {
-
-    }
-    else if (flowerHealth === 1) {
-
-    }
-    else {
-        killFlower();
-    }
+    updateDisplay();
 }
 
 // write a function that increases health by 1 and changes the image
 function water() {
     flowerHealth++;
-    if (flowerHealth === 2) {
+    updateDisplay();
+}
 
+// write a compliment function that makes the flower temporarily happy, then reverts to its old state
+function compliment() {
+    flower.style.backgroundPosition = "0px";
+    setTimeout(function () {
+        updateDisplay();
+    }, 2000)
+
+}
+
+
+function killFlower() {
+    flower.classList.add("dead");
+}
+
+function updateDisplay() {
+    if ( flowerHealth >= 4 ) {
+        flower.style.backgroundPosition = "-100px";
     }
     else if (flowerHealth === 3) {
-
-    }
-    else if (flowerHealth === 4) {
-
-    }
+        flower.style.backgroundPosition = "-200px";
+    }  
+    else if (flowerHealth === 2) {
+        flower.style.backgroundPosition = "-300px";
+    } 
+    else if (flowerHealth === 1) {
+        flower.style.backgroundPosition = "-400px";
+    } 
     else {
         killFlower();
     }
 }
 
-// write a compliment function that makes the flower temporarily happy, then reverts to its old state
-function compliment() {
-
-}
-
-function killFlower() {
-
-}
+document.getElementById("neglect").addEventListener("click", neglect);
+document.getElementById("water").addEventListener("click", water);
+document.getElementById("compliment").addEventListener("click", compliment);
